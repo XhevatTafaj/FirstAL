9262175bf2d405d06a6cbdff65a242d4f92cfe19
Xhevat Tafaj <xtafaj@alteanet.it>
2019-06-07 15:36:18 +0200

    07/06/2019 First

---
+++ .vscode/PowerShell.ps1
@@ -0,0 +1,5 @@
+Import-Module 'C:\Program Files\Microsoft Dynamics NAV\110\Service\NavAdminTool.ps1'
+Import-Module 'C:\Program Files (x86)\Microsoft Dynamics NAV\110\RoleTailored Client\NavModelTools.ps1'
+
+
+
